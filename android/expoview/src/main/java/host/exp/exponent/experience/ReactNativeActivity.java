801a6cec4c1972b3e65cc533d55550fd1e4ba221
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2018-10-10 16:22:55 +0200

    [android] singleton modules

--- android/expoview/src/main/java/host/exp/exponent/experience/ReactNativeActivity.java
+++ android/expoview/src/main/java/host/exp/exponent/experience/ReactNativeActivity.java
@@ -416,6 +416,7 @@ public abstract class ReactNativeActivity extends FragmentActivity implements co
     instanceManagerBuilderProperties.expoPackages = extraExpoPackages;
     instanceManagerBuilderProperties.exponentPackageDelegate = delegate.getExponentPackageDelegate();
     instanceManagerBuilderProperties.manifest = mManifest;
+    instanceManagerBuilderProperties.singletonModules = ExponentPackage.createSingletonModules(getApplicationContext());
 
     RNObject versionedUtils = new RNObject("host.exp.exponent.VersionedUtils").loadVersion(mSDKVersion);
     RNObject builder = versionedUtils.callRecursive("getReactInstanceManagerBuilder", instanceManagerBuilderProperties);
